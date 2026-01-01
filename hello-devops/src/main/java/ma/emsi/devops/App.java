2c06edc39b378941f78e0696aef7a5a56708f767
benyacoub akram <akrambenyacoub2@gmail.com>
2026-01-01 15:16:01 +0100

    Add simple Java app DevOps

---
+++ hello-devops/src/main/java/ma/emsi/devops/App.java
@@ -0,0 +1,8 @@
+package ma.emsi.devops;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Bonjour et bon courage dans votre projet en DevOps");
+    }
+}
+
